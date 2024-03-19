b543725c0d913ab2ee79ac8b0afadcfde3cb65ef
Wim Bervoets <wim.bervoets@pxl.be>
2024-03-19 15:09:16 +0100

    Add project files.

---
+++ HrApp/Controllers/EmployeeController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using HrApp.Data;
+using HrApp.Models;
+using HrApp.Repositories;
+using HrApp.ViewModels;
+
+namespace HrApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public EmployeeController(IEmployeeRepository repository)
+        {
+            _employeeRepository = repository;
+        }
+
+        // GET: Employee
+        public async Task<IActionResult> Index()
+        {
+            var employees = await _employeeRepository.GetAll();
+            return View(employees);
+              //return _context.Employees != null ? 
+              //            View(await _context.Employees.ToListAsync()) :
+              //            Problem("Entity set 'AppDbContext.Employee'  is null.");
+        }
+
+        // GET: Employee/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return BadRequest();
+            }
+
+            var employee = await _employeeRepository.GetById(id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return View(employee);
+        }
+
+        // GET: Employee/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Employee/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("EmployeeId,FirstName,LastName")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                await _employeeRepository.Add(employee);
+
+                return RedirectToAction(nameof(Index));
+            }
+            return View(employee);
+        }
+
+        // GET: Employee/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return BadRequest();
+            }
+
+            var employee = await _employeeRepository.GetById(id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: Employee/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("EmployeeId,FirstName,LastName")] Employee employee)
+        {
+            if (id != employee.EmployeeId)
+            {
+                return BadRequest();
+            }
+
+            if (ModelState.IsValid)
+            {
+                await _employeeRepository.Update(employee);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(employee);
+        }
+
+        // GET: Employee/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return BadRequest();
+            }
+
+            var employee = await _employeeRepository.GetById(id);
+                
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return View(employee);
+        }
+
+        // POST: Employee/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var employee = await _employeeRepository.GetById(id);
+            
+            if (employee != null)
+            {
+                await _employeeRepository.Delete(employee);
+            }
+            
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Search([FromForm] string firstName)
+        {
+            var employees = await _employeeRepository.GetAll();
+
+            var model = new SearchEmployeeViewModel();
+
+            if (!string.IsNullOrEmpty(firstName))
+                model.Employees = employees.Where(e => e.FirstName.Contains(firstName, StringComparison.OrdinalIgnoreCase)).ToList();
+
+            return View(model);
+        }
+    }
+}
---
+++ HrApp/ViewModels/SearchEmployeeViewModel.cs
@@ -0,0 +1,10 @@
+﻿using HrApp.Models;
+
+namespace HrApp.Models
+{
+    public class SearchEmployeeViewModel
+    {
+        public string FirstName { get; set; }
+        public List<Employee> Employees { get; set; } = new List<Employee>();
+    }
+}
